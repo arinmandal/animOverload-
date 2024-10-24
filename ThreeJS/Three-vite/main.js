5f2b1f5aa701cd188458be115f4d378bda708fe0
Arin <arinmandal40@gmail.com>
2024-10-24 20:03:50 +0530

    How to add texture and light

--- ThreeJS/Three-vite/main.js
+++ ThreeJS/Three-vite/main.js
@@ -10,11 +10,38 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 // scene.add(cube);
 
 // sphere
-const geometry = new THREE.SphereGeometry( 1, 10, 10 ); 
-const material = new THREE.MeshBasicMaterial( { color: 'crimson', wireframe: true } ); 
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
+// const geometry = new THREE.SphereGeometry( 1, 10, 10 );
+// const material = new THREE.MeshBasicMaterial( { color: 'crimson', wireframe: true } );
+// const sphere = new THREE.Mesh(geometry, material);
+// scene.add(sphere);
+// camera.position.z = 5;
+
+// Studio Lighting Setup
+const light1 = new THREE.DirectionalLight(0xffffff, 1);
+light1.position.set(1, 2, 3); // Position light to the right and above
+scene.add(light1);
+
+const light2 = new THREE.DirectionalLight(0xffffff, 1);
+light2.position.set(-1, 2, -3); // Position light to the left and behind
+scene.add(light2);
+
+const light3 = new THREE.AmbientLight(0xffffff, 0.5); // Soft ambient light
+scene.add(light3);
+
+// Texture
+let loader = new THREE.TextureLoader();
+let color = loader.load("./paperbox/color.jpg");
+let roughness = loader.load("./paperbox/roughness.jpg");
+let normal = loader.load("./paperbox/normal.jpg");
+let height = loader.load("./paperbox/height.jpg");
+
+// how to add texture to scene
+const geometry = new THREE.BoxGeometry(2, 1, 2);
+const material = new THREE.MeshStandardMaterial({ map: color, roughness: roughness, normalMap: normal});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 camera.position.z = 5;
+
 
 const canvas = document.querySelector('canvas');
 const renderer = new THREE.WebGLRenderer({ canvas });
@@ -31,8 +58,8 @@ controls.enableDamping = true;
 // controls.autoRotate = true;
 function animate() {
   window.requestAnimationFrame(animate)
-  sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.01;
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
   controls.update();
   renderer.render(scene, camera);
 
