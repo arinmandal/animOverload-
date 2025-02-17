57b04a0ad0ba3f82599e35bdff65f214056b7d1b
Arin <arinmandal40@gmail.com>
2025-02-18 00:33:42 +0530

    Animate with Keyframes in Motion

--- FramerMotion/src/App.jsx
+++ FramerMotion/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 const App = () => {
   return (
     <>
-      <motion.img
+      {/* <motion.img
         animate={{ x: 1000 }}
         transition={{
           delay: 1,
@@ -12,7 +12,23 @@ const App = () => {
           ease: "anticipate",
           // repeat: Infinity
         }}
-        src="https://pngimg.com/d/tom_and_jerry_PNG29.png" alt="" />
+        src="https://pngimg.com/d/tom_and_jerry_PNG29.png" alt="" /> */}
+
+      <motion.div
+        initial={{ x: 0, y: 0 }}
+        animate={{
+          x: [0, 700, 700, 0, 0],
+          y: [0, 0, 500, 500, 0]
+        }}
+
+        transition={{
+          delay: 1,
+          duration: 5,
+
+        }}
+        className="box">
+
+      </motion.div>
     </>
   )
 }
