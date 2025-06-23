d533de792a61f1f5c5cf3a15dd5387651a1126f9
Arin <arinmandal40@gmail.com>
2025-06-23 11:55:16 +0530

    Updated with all components

--- ModernWebCraft/Mojito/src/App.jsx
+++ ModernWebCraft/Mojito/src/App.jsx
@@ -1,14 +1,25 @@
-import { ScrollTrigger, SplitText } from "gsap/all"
-import gsap from "gsap"
-
-gsap.registerPlugin(ScrollTrigger, SplitText)
-
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+gsap.registerPlugin(ScrollTrigger, SplitText);
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
+import About from "./components/About";
+import Art from "./components/Art";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1>Mojito</h1>
-    </div>
-  )
-}
+    <main>
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact/>
+    </main>
+  );
+};
 
-export default App
+export default App;
